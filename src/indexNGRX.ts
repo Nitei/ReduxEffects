19370f04a9f1d14a54dde608621762890a713ea3
Jorge <jorgecampoy81@gmail.com>
2019-05-28 14:00:17 +0200

    Agregada lista de pasos de ngrx

---
+++ src/indexNGRX.ts
@@ -0,0 +1,16 @@
+/*
+1 Crear carpetas actions, effects, reducers
+2 ACCIONES
+  2.1 Crear constantes de las acciones
+  2.2 Crear las acciones
+  2.3 Exportar tipos de las acciones
+3 REDUCER
+  3.1 Crear la interfaz del state
+  3.2 Crear constate con el estado inicial de el state
+  3.3 Crear el reducer que modificará el state
+4 Crear el app.reducers
+  4.1 Crear la interfaz del store "AppState"
+  4.2 Crear el actionReducerMap que es la combinación de todos los
+     reducers que usa la interface de "AppState"
+
+*/
