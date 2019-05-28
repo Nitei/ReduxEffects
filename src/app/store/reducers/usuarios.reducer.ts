f65585c24611b1e9693b428a1eccd9753b738d01
Jorge <jorgecampoy81@gmail.com>
2019-05-28 13:33:12 +0200

    Agregado reducer function e index.ts

--- src/app/store/reducers/usuarios.reducer.ts
+++ src/app/store/reducers/usuarios.reducer.ts
@@ -1,7 +1,7 @@
 import { Usuario } from 'src/app/models/usuario.model';
 import * as fromUsuarios from '../actions/index';
 
-// Aquí definimos como queremos que luzca el state
+// 3 Aquí definimos como queremos que luzca el state
 export interface UsuariosState {
   users: Usuario[];
   loaded: boolean;
@@ -9,10 +9,40 @@ export interface UsuariosState {
   error: any;
 }
 
-// Inicializamos los parámetros del state
+// 4 Inicializamos los parámetros del state
 const estadoInicial: UsuariosState = {
   users: [],
   loaded: false,
   loading: false,
   error: null
+}
+
+// 5 Creamos el reducer
+export function usuariosReducer( state = estadoInicial, action: fromUsuarios.usuarioAcciones ) {
+  switch ( action.type ) {
+    case fromUsuarios.CARGAR_USUARIOS:
+      return {
+        ...state,
+        loading: true
+      }
+
+    case fromUsuarios.CARGAR_USUARIOS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        users: [ ...action.usuarios ]
+      }
+
+    case fromUsuarios.CARGAR_USUARIOS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        users: action.payload
+      }
+
+    default:
+      return state;
+  }
 }
