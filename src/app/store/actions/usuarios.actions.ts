adab4b1b335a154d95f9ead7c67982e7d2035b35
Jorge <jorgecampoy81@gmail.com>
2019-05-28 13:08:14 +0200

    Creadas constante en las acciones

---
+++ src/app/store/actions/usuarios.actions.ts
@@ -0,0 +1,4 @@
+export const CARGAR_USUARIOS = '[Usuarios] Cargar usuarios';
+export const CARGAR_USUARIOS_FAIL = '[Usuarios] Cargar usuarios FAIL';
+export const CARGAR_USUARIOS_SUCCESS = '[Usuarios] Cargar usuarios SUCCESS';
+
