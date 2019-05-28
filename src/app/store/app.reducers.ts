23dd1c8321e84209face4857c85beae9c822b76d
Jorge <jorgecampoy81@gmail.com>
2019-05-28 13:48:00 +0200

    Agregado app.reducers.ts

---
+++ src/app/store/app.reducers.ts
@@ -0,0 +1,13 @@
+import * as reducers from './reducers';
+import { ActionReducerMap } from '@ngrx/store';
+
+
+// 7 Como queremos que luzca el store
+export interface AppState {
+  usuarios: reducers.UsuariosState;
+}
+
+// 8 Combinacion de todos los reducers que usa la interface de AppState
+export const appReducers: ActionReducerMap<AppState> = {
+  usuarios: reducers.usuariosReducer
+}
