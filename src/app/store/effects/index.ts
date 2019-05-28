f89ac802f698400d3e43da51bb3010c3ffd485c5
Jorge <jorgecampoy81@gmail.com>
2019-05-28 18:56:51 +0200

    Agregado index de efectos

---
+++ src/app/store/effects/index.ts
@@ -0,0 +1,6 @@
+import { UsuariosEffects } from './usuarios.effects';
+
+// 10 Creamos un array con todos los efectos y lo servimos
+export const effectsArr: any[] = [ UsuariosEffects ];
+
+export * from './usuarios.effects';
